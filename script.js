bbca5732162f7604c87f547eeefe8a42565f7a2a
kev <nikcastro2006@gmail.com>
2025-03-29 13:22:42 -0300

    we cheeeered

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function clickMenu() {
-  if (menuHamburger.style.display == "none") {
-     menuHamburger.style.display = "block";
-  } else menuHamburger.style.display == "none";
-  
+//function clickMenu() {
+//  if (menuHamburger.style.display == "none") {
+//    menuHamburger.style.display = "block";
+//  } else menuHamburger.style.display == "none";
+//}
+
+//function showSection(section){
+//  const item = document.querySelectorAll('.section');
+//  item.forEach((s) => s.classList.remove('active'));
+
+//  const target = document.getElementById(id);
+//  if (target) target.classList.add('active');
+//}
+
+function showSection(section) {
+  document
+    .querySelectorAll("section")
+    .forEach((sec) => sec.classList.remove("active"));
+
+  document
+    .querySelectorAll(`.${section}`)
+    .forEach((sec) => sec.classList.add("active"));
 }
